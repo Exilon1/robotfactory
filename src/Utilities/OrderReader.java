a12c568c8431ba1596ee1659e66852448ebab733
Alexey <delexyz@mail.ru>
2016-11-02 11:33:33 +0400

    Development

--- src/Utilities/OrderReader.java
+++ src/Utilities/OrderReader.java
@@ -39,7 +39,8 @@ public class OrderReader {
         boolean isNeedSmokeCar = "да".equals(params[3].toLowerCase());
         int needCarClass = Integer.valueOf(params[4]);
 
-        return new Order(startPoint, endPoint, isNeedBabySeat, isNeedSmokeCar, needCarClass);
+    //    return new Order(startPoint, endPoint, isNeedBabySeat, isNeedSmokeCar, needCarClass);
+        return null;
     }
 
 
