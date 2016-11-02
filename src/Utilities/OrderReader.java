b0d5048912a1fa81100d0b2e6a876d85ea116af3
Alexey <delexyz@mail.ru>
2016-11-02 11:19:31 +0400

    Development

--- src/Utilities/OrderReader.java
+++ src/Utilities/OrderReader.java
@@ -1,6 +1,10 @@
 package Utilities;
 
 import Entity.Order;
+import exceptions.NotCompleteRequestException;
+import exceptions.NotValidCarClassException;
+import exceptions.UndetectableBabySeatParamException;
+import exceptions.UndetectableSmokeCarParamException;
 
 /**
  * Created by Alexey on 30.10.2016.
@@ -18,8 +22,27 @@ public class OrderReader {
         return SingletonHelper.SINGLETON;
     }
 
-    public static Order orderRead(String request) {
+    public static Order orderRead(String request) throws Exception {
         String[] params = request.split("|");
+        if(params.length<5)
+            throw new NotCompleteRequestException();
+        if(!"да".equals(params[2].toLowerCase()) && !"нет".equals(params[2].toLowerCase()))
+            throw new UndetectableBabySeatParamException();
+        if(!"да".equals(params[3].toLowerCase()) && !"нет".equals(params[3].toLowerCase()))
+            throw new UndetectableSmokeCarParamException();
+        if(!Integer.toString(0).equals(params[4]) && !Integer.toString(1).equals(params[4]))
+            throw new NotValidCarClassException();
+
+        String startPoint = params[0];
+        String endPoint = params[1];
+        boolean isNeedBabySeat = "да".equals(params[2].toLowerCase()) ? true : false;
+        boolean isNeedSmokeCar = "да".equals(params[3].toLowerCase()) ? true : false;
+        int needCarClass = Integer.valueOf(params[4]);
+
+        Order order = new Order(startPoint, endPoint, isNeedBabySeat, isNeedSmokeCar, needCarClass);
+
+        //TODO
+
         return null;
     }
 
---
+++ src/storage/CarList.java
@@ -0,0 +1,26 @@
+package storage;
+
+import Entity.Car;
+import Utilities.OrderReader;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Nikotin on 02.11.2016.
+ */
+public class CarList extends ArrayList<Car> {
+
+    private CarList() {
+        super(10);
+    }
+
+    private static class SingletonHelper {
+        private static final CarList SINGLETON = new CarList();
+    }
+
+    public static CarList getInstance() {
+        return CarList.SingletonHelper.SINGLETON;
+    }
+
+
+}
